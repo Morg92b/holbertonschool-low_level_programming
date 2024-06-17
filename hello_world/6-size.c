f01b159a43a09cde7df9915b99a837bcb456486f
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-17 15:04:44 +0200

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types on the computer it is compiled
+ * Return:0
+ */
+
+int main(void)
+
+	printf(char);
+	printf(int);
+	printf(long int);
+	printf(long long int);
+	printf(float);
+
+	return (0);
+}
