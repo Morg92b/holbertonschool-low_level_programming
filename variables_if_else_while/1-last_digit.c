db53a81e8cda0680d2e899357d45aa8bfba05bb1
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-17 18:16:08 +0200

    Greater or Less

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,12 +16,12 @@ int main(void)
 	printf("Last digit of %d is", n);
 	n %= 10;
 	if (n > 5)
-		printf("%d and is greater than 5\n", n);
+		printf(" %d and is greater than 5\n", n);
 	n %= 10;
 	if (n == 0)
-		printf("%d and is 0\n", n);
+		printf(" %d and is 0\n", n);
 	n %= 10;
 	if (n < 6 && n != 0)
-		printf("%d and is less than 6 and not 0\n", n);
+		printf(" %d and is less than 6 and not 0\n", n);
 	return (0);
 }
