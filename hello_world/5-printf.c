1a59327a9ab73d8104ca43906cb6b1daa8eccb68
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-17 13:58:47 +0200

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,7 +5,6 @@
  * main - prints 'with proper grammar, but the outcome is a piece of art,'
  * Return:0
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
