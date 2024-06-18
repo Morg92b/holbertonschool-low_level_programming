cb6be684641890c800560551f6aea260717e3544
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-18 11:46:00 +0200

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet with putchar and char c i use condition.
+ * Return:0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+
+	putchar(c++);
+
+	putchar('\n');
+
+
+	return (0);
+}
+
