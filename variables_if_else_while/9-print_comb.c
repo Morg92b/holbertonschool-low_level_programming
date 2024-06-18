fc325b7c792bfc8471d1f61dab920b996bcf2e11
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-18 18:11:12 +0200

    task9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return:0
+ */
+
+int main(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+
+	{
+		if (n < ':')
+
+			putchar(n++);
+			putchar(',');
+			putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
