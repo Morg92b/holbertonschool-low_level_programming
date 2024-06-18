bcdd92379aad60732d64105482f077ff5b3e31b6
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-18 13:37:37 +0200

    alphabetxuppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+
+	putchar(c++);
+	{
+
+	char C = 'A';
+
+	while (C <= 'Z')
+
+	putchar(C++);
+
+	}
+
+	putchar('\n');
+	return (0);
+}
