e6ae053b13b7f33658214b7d90ef7ef003beabdb
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-19 14:30:09 +0200

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - i print alphabet with _putchar and i created a new files
+ * Return:void
+ */
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+
+	_putchar(c++);
+
+	_putchar('\n');
+
+}
