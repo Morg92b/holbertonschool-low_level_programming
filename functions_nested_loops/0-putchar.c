cba7962c45f289fbea2b5137d4a7890eb0c2796f
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-19 13:13:48 +0200

    task0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/** main - i print _putchar with files main.h i created the commands
+ * Return:0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
+
