cb5a4c51e65073ebf3eea4842f1160a4d85a101f
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-19 15:08:01 +0200

    task2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - i print x10 alphabet
+ * Return:void
+ */
+
+void print_alphabet_x10(void)
+{
+        char c = 'a';
+
+        while (c <= 'z')
+
+        _putchar(c++);
+
+        _putchar('\n');
+
+}
