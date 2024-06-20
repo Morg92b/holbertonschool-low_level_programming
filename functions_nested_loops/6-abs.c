d1a470f54fcbcb1de2fc89cdbcea6576a7228073
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-20 15:33:11 +0200

    task6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @r: the character used to be checked
+ * Return: r && -r
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	return (r);
+	else
+	return (-r);
+}
