0436fa0df59fa5bdc80b70ecc3feb92d0b2dc98f
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-24 17:41:27 +0200

    task9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -5,9 +5,9 @@ int main(void)
 {
 	printf("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz ");
 	printf("13 14 FizzBuzz 16 17 Fizz ");
-	printf("19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz");
-	printf("31 32 Fizz 34 Buz");
-	printf("z Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz ");
+	printf("19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz ");
+	printf("31 32 Fizz 34 Buzz");
+	printf(" Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz ");
 	printf("46 47 Fizz 49 Buzz Fizz");
 	printf("52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz");
 	printf(" 64 Buzz Fizz 67 68");
