bae60893d4188de0d1b8b7616d77946c4af1b2d9
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-24 16:07:15 +0200

    task6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- *
+ *print_line- that draws a straight line in the terminal
+ *@n:is the number of times the character
  */
 
 void print_line(int n)
 {
 	int i;
- for (i = 0 ; i < n; i++)
- {
-	 _putchar('_');
- }
- _putchar('\n');
+
+	for (i = 0 ; i < n; i++)
+
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
