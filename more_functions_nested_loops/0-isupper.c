1d78cb3e77f933cdb12e17012e374fc8217fe2a9
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-24 11:11:46 +0200

    task0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
-*
-*
-*/
+ *_isupper - checks for uppercase character
+ *@c: it is the value that determines
+ *Return:1 and 0
+ */
+
+
 
 int _isupper(int c)
 {
-
-  if (c >= 'A' && c <= 'Z')
-  {
-    return (1);
-  }
-  else
-  {
-    return (0);
-  }
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
 }
