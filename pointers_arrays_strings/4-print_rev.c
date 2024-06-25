8a3f221e9e64de675601fccf64f14fc8eae1c30a
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-25 16:16:13 +0200

    task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev- that prints a string in reverse
+ *@s: it is the result
+ */
+
+void print_rev(char *s)
+{
+	int longueur = 0;
+
+	while (s[longueur] != '\0')
+	{
+	longueur++;
+	}
+
+	while (longueur >= 0)
+	{
+		_putchar(s[longueur]);
+		longueur--;
+	}
+	_putchar('\n');
+}
