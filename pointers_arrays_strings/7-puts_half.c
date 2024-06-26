cf1f80bf52774f3827dad4d13e5a445467942e81
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-26 17:50:01 +0200

    task7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,20 +1,32 @@
 #include "main.h"
 
 /**
- *
- *
+ *puts_half- function that prints half of a string
+ *@str: it is the string
  */
 
 void puts_half(char *str)
 {
-	int longueur = 5;
+	int index = 0;
+	int line = 0;
 
-	while (str[longueur] != '\0')
+	while (str[line] != '\0')
 	{
-	_putchar(str[longueur]);
-	longueur++;
+		line++;
 	}
-
+	if (line % 2 == 0)
+	{
+		index = line / 2;
+	}
+	else
+	{
+		index = (line / 2) + 1;
+	}
+	while (index < line)
+	{
+		_putchar(str[index]);
+		index++;
+	}
 	_putchar('\n');
 }
 
