4ca057a610634278ac4e036f7c22c6ed61d1e33f
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-27 10:58:48 +0200

    task9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -5,13 +5,14 @@
  *_strcpy- function that copies the string pointed
  *@dest: it is the value cp
  *@src: it is the value
+ *Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int index;
 
-	for (index= 0; src[index] != '\0'; index++)
+	for (index = 0; src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
 		dest[i] = '\0';
