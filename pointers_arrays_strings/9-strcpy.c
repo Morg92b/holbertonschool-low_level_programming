bda4c23c87eaf3b3a1165ff58fbabe90617cacb7
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-27 11:00:16 +0200

    task9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -15,9 +15,9 @@ char *_strcpy(char *dest, char *src)
 	for (index = 0; src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
-		dest[index] = '\0';
 	}
-		return (dest);
+	dest[index] = '\0';
+	return (dest);
 }
 
 
