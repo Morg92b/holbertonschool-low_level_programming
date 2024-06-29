768161c390c2faf120a579fc23e7faa900c4690c
Morg92b <morgan.bouaziz@outlook.fr>
2024-06-29 14:10:40 +0200

    add task6 work

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,8 +2,9 @@
 #include <ctype.h>
 
 /**
- *
- *
+ *cap_string- function that capitalizes all words of a string
+ *@p: pointer of string
+ *Return: p
  */
 
 char *cap_string(char *p)
@@ -12,7 +13,8 @@ char *cap_string(char *p)
 
 	while (p[index] != '\0')
 	{
-		if ((index == 0 || p[index - 1] == ' ' || p[index - 1] == '\t' || p[index - 1] == '.' || p[index - 1] == '\n') 
+		if ((index == 0 || p[index - 1] == ' ' || p[index - 1] == '\t' ||
+		p[index - 1] == '.' || p[index - 1] == '\n')
 		&& p[index] >= 'a' && p[index] <= 'z')
 
 		p[index] -= 32;
