6c1992c48e530ae6e0121bf50fa0447d52949ddf
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-01 11:02:02 +0200

    add task0 memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_memset- that fills memory with a constant byte
+ *@n: it is the valor octets of memory
+ *@b: it is the constant value of memory
+ *@s: it is the pointer
+ *Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int index;
+
+	for (index = 0; index < n; i++)
+
+		s[index] = n;
+	return (s);
+}
