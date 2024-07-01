f697a04025b852da827fa2d08842a06488a6de98
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-01 11:41:38 +0200

    add task2 strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strchr- that locates a character in a string
+ *@s:it is the pointer
+ *@c:it is the character
+ *Return: s & 0
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+
+		s++;
+	}
+	return (0);
+}
+
