f8e672a803abf3dfad8f427c51e2af0ba15a6429
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-01 14:45:33 +0200

    add task5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,15 +10,19 @@
 char *_strstr(char *haystack, char *needle)
 {
 	if (*needle == '\0')
-		return (haystack);
+		return haystack;
+
 	while (*haystack)
 	{
-		while (*haystack == *needle)
+		char *h= haystack;
+		char *n = needle;
+
+		while (*h && *n && (*h == *n))
 		{
-			haystack++;
-			needle++;
+			h++;
+			n++;
 		}
-		if (*needle == '\0')
+		if (*n == '\0')
 
 		return (haystack);
 
