53062a8d28f285a19c2e4846a6ef9c1a707aea7c
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-01 15:34:28 +0200

    add task 8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *print_diagsums- prints the sum of diagonals
+ *@a: it is the pointer
+ *@size: size
+ *
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum_1 = 0;
+	int sum_2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
