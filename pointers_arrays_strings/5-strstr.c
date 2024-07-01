58fa65a5f65389df65d7cc5bbd5ca805d98f415f
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-01 14:26:38 +0200

    add task5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strstr- function that locates a substring
+ *@haystack: it is the full string
+ *@needle: it is the pointer to search in haystack
+ *Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	while (*haystack)
+	{
+		while (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+		if (*needle == '\0')
+			return (haystack);
+
+		haystack++;
+	}
+	return (0);
+}
