6a695d6deff0b31786adebaa49851a246a43c598
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-03 10:28:13 +0200

    add task5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ *_square_i- it is the recurses find the natural
+ *@a: it is the variable
+ *@i: it is the square
+ *Return: result
+ */
+
+int _square_i(int a, int i)
+{
+	if (i * i == a)
+	{
+		return (i);
+	}
+
+	if (a / 2 == i)
+	{
+		return (-1);
+	}
+	else
+		return (_square_i(a, i + 1));
+}
+
+/**
+ * _sqrt_recursion -returns the natural square
+ * @n: number calculate
+ *Return: resultat
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	else
+		return (_square_i(n, 1));
+
