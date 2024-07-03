07f8175ee69bbb0d413d55d8a28db3a777b21f08
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-03 14:15:58 +0200

    add task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- prints the number of arguments
+ *@argc: argument count
+ *@argv:array of pointers to strings
+ *Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%s\n", argc);
+	return (0);
+}
