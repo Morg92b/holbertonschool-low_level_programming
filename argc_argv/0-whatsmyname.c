15fa5eb948412b04c8128b6ba149129db8449cb2
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-03 13:45:22 +0200

    add task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- parameter argc & argv
+ *@argc:counting the number of command line arguments passed to the program
+ *@argv:array of pointers to strings
+ *Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	return (0);
+}
