904e0523ba8726d63bc7e09f51b7136110221834
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-05 09:09:12 +0200

    add task4 & finish project

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 		printf("0\n");
 			return (1);
 	}
-	for (index_1 = 1; i < argc; i++)
+	for (index_1 = 1; index_1 < argc; index_1++)
 	{
 		for (index_2 = 0; argv[index_1][index_2] != '\0'; index_2++)
 
