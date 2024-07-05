d4a80255eb3cf8547b379b3d7f5fb8e97a8de4f0
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-05 09:06:13 +0200

    add task4 & finish project

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- prints the number of arguments
+ *@argc: argument count
+ *@argv:array of pointers to strings
+ *Return: 1 if result 0 or error and return 0 if result is > 0
+ */
+
+int main(int argc, ²char **argv)
+{
+	int index_1;
+	int index_2;
+	int result = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+			return (1);
+	}
+	for (index_1 = 1; i < argc; i++)
+	{
+		for (index_2 = 0; argv[index_1][index_2] != '\0'; index_2++)
+
+			if (argv[index_1][index_2] < '0' || argv[index_1][index_2] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result)
+	return (0);
+}
+
+
+
