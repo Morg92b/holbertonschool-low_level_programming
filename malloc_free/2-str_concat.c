651f6d326481325229ff8a2e2eccf2c278b6f2ec
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-08 19:52:49 +0200

    add task 2 concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,11 +18,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		return ("");
+		s2 = "";
 	}
 
 	while (s1[length_1] != '\0')
