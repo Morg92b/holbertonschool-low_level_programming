4c52042571c03a90203f01646890f9963a965b67
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-08 11:15:23 +0200

    fix main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void reset_to_98(int *n);
 
 int _putchar(char c);
@@ -37,3 +40,21 @@ char *string_toupper(char *);
 char *cap_string(char *);
 
 char *leet(char *);
+
+char *_memset(char *s, char b, unsigned int n);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_strchr(char *s, char c);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strpbrk(char *s, char *accept);
+
+char *_strstr(char *haystack, char *needle);
+
+void print_chessboard(char (*a)[8]);
+
+void print_diagsums(int *a, int size);
+
+#endif
