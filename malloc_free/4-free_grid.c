3934b8443c5955a1e440f124a9f751600c854ff7
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-09 12:17:09 +0200

    add task 4 free memory of 2 dimensional grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *free_grid - free allocated memory for grid
+ *@grid: 2 dimensional grid
+ *@height: height of the grid
+ */
+
+
+void free_grid(int **grid, int height)
+{
+	int index = 0;
+
+	if (grid == NULL || height <= 0)
+		free(grid[index]);
+	free(grid);
+}
