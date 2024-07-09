828c92c2af902f0f1b1f38007cc82950ab0c214f
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-09 16:03:02 +0200

    add task 0 function malloc_checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *malloc_checked - allocates memory using malloc
+ *@b: it is the size of memory 
+ *Return: ptr or exit(98)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	void ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	free(ptr);
+	return (ptr);
+}
+
