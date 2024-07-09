826fc7045df388b8b1e67799f6a204b96c0016c0
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-09 16:13:41 +0200

    added task 0 fix

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -8,15 +8,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
-
-	void ptr = malloc(b);
+	void *ptr = malloc(b);
 
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
-	free(ptr);
 	return (ptr);
 }
 
