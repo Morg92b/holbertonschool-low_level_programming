5674ee52c79733cc343b0efd8dcf59d32d693c99
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-17 10:28:02 +0200

    added task 0 that returns the sum of all its paremeters

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,18 +2,28 @@
 
 /**
  *sum_them_all - that returns the sum of all its parameters
- *
+ *@n: the number of integers passed to the function
+ *Return: sum
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int index = n;
+	unsigned int index;
+	int sum = 0;
 	va_list ap;
+
 	if (n == 0)
+	{
 		return (0);
+	}
+
 	va_start(ap, n);
-	for (; index >= 0; index = va_arg(ap,const unsigned int))
-		va_end(ap);
-	return (index);
+
+	for (index = 0; index < n; index++)
+	{
+		sum += va_arg(ap, unsigned int);
+	}
+	va_end(ap);
+	return (sum);
 }
 
