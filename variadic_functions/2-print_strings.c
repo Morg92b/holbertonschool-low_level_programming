509c7b8074738d74682820675d3135f0048eddf5
Morg92b <morgan.bouaziz@outlook.fr>
2024-07-17 12:13:33 +0200

    added task 2 that prints strings, followed by a new line

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- *print_strings - 
+ *print_strings - that prints strings, followed by a new line
  *@separator: is the string to be printed between numbers
  *@n: is the number of integers passed to the function
  */
@@ -17,17 +17,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (index = 0; index < n; index++)
 	{
 		str = va_arg(ap, char*);
-	
+
 	if (str == NULL)
-	
+
 		printf("(nil)");
 	else
 		printf("%s", str);
-	
-	if ( separator != NULL && index < n -1)
-	
+
+	if (separator != NULL && index < n - 1)
+
 		printf("%s", separator);
-	
 	}
 	putchar('\n');
 	va_end(ap);
