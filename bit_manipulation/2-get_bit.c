8e4213f66c4fc2c241871e5ce7d31bd832f25ffb
Morg92b <morgan.bouaziz@outlook.fr>
2024-08-01 16:51:57 +0200

    fix function

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -6,7 +6,7 @@
  * @index: it is position of bit 
  */
 
-int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
