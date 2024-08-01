df6fd3db08cbb2f5f53fc5eb7c1fc00e032ebbff
Morg92b <morgan.bouaziz@outlook.fr>
2024-08-01 16:58:25 +0200

    fix betty

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -3,7 +3,8 @@
 /**
  * get_bit - returns the value of a bit at a given index
  * @n: decimal
- * @index: it is position of bit 
+ * @index: it is position of bit
+ * Return: the value of the bit at index or -1 if an error
  */
 
 int get_bit(unsigned long int n, unsigned int index)
