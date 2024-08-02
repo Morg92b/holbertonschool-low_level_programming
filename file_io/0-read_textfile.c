60da195f91ef5f778b45f8fa2e0e58edf7726a07
Morg92b <morgan.bouaziz@outlook.fr>
2024-08-02 14:09:16 +0200

    fix betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: Text file
+ * @letters: numbers of letters
+ * Return: the actual number of letters it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
