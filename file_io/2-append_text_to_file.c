31c026e1d6c9e9d9540c186169570cce050983cb
Morg92b <morgan.bouaziz@outlook.fr>
2024-08-02 17:45:11 +0200

    fix condition

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,7 +10,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int file_descriptor;
 	ssize_t bytes_written;
-	int len;
+	int len = 0;
 
 	if (filename == NULL)
 	{
@@ -28,9 +28,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	for (len = 0; text_content[len]; len++)
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
 
 	bytes_written = write(file_descriptor, text_content, len);
+
 	if (bytes_written == -1)
 	{
 		close(file_descriptor);
