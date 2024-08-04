69fb7cffb98cd2a1804ba7d3bbe586dd0dbcf343
Morg92b <morgan.bouaziz@outlook.fr>
2024-08-04 20:24:58 +0200

    fix the function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,54 +3,56 @@
 /**
  * print_error - print a error message and exit the program
  * with a error code
- * @msg: it is the string error format
- * @file:it is the name of file error
- * @code: that error code
+ * @src: it's source file
+ * @dest:it's destination file
+ * @argv: argument vector
  */
 
-void print_error(const char *msg, const char *file, int code)
+void print_error(int src, int dest, char *argv[])
 {
-	dprintf(STDERR_FILENO, "%s%s\n", msg, file);
-	exit(code);
-}
-/**
- * copy_file - That copy file source to destination
- * @src_filename: it's a pointer to a string character of name file source
- * @dest_filename: it's a pointer to a string character
- * of name file destination
- */
-void copy_file(const char *src_filename, const char *dest_filename)
-{
-	char buffer[1024];
-	ssize_t bytes_read;
-	int src, dest;
-
-	src = open(src_filename, O_RDONLY);
 	if (src == -1)
-		print_error("Error: Can't read from file %s\n", src_filename, 98);
-
-	dest = open(dest_filename, O_WRONLY | O_CREAT | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (dest == -1)
 	{
-		close(src);
-		print_error("Error: Can't write to %s\n", dest_filename, 99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
-	while ((bytes_read = read(src, buffer, sizeof(buffer))) > 0)
+}
+/**
+ * main - check the code
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Alaways 0
+ */
+int main(int argc, char *argv[])
+{
+	char buffer[1024];
+	int src, dest;
+	ssize_t bytes_read, bytes_written;
+
+	if (argc != 3)
 	{
-		if (write(dest, buffer, bytes_read) != bytes_read)
-		{
-			close(src);
-			close(dest);
-			print_error("Error: Can't write to %s\n", dest_filename, 99);
-		}
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
 	}
 
-	if (bytes_read == -1)
+	src = open(argv[1], O_RDONLY);
+
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+	bytes_read = 1024;
+	while (bytes_read == 1024)
 	{
-		close(src);
-		close(dest);
-		print_error("Error: can't read from file %s\n", src_filename, 98);
+		bytes_read = read(src, buffer, 1024);
+		if (bytes_read == -1)
+			print_error(-1, 0, argv);
+		bytes_written = write(dest, buffer, bytes_read);
+		if (bytes_written == -1)
+			print_error(0, -1, argv);
 	}
 
 	if (close(src) == -1 || close(dest) == -1)
@@ -59,21 +61,5 @@ void copy_file(const char *src_filename, const char *dest_filename)
 				(close(src) == -1 ? src : dest));
 		exit(100);
 	}
-}
-/**
-  * main - check the code
-  * @argc: Argument count
-  * @argv: Argument vector
-  * Return: Alaways 0
-  */
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
-	}
-
-	copy_file(argv[1], argv[2]);
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -12,6 +12,5 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_error(const char *msg, const char *file, int code);
-void copy_file(const char *src_filename, const char *dest_filename);
+void print_error(int src, int dest, char *argv[]);
 #endif /* MAIN_H */
